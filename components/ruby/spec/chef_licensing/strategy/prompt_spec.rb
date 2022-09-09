a7d4723d8600786c6abfcc3783b9011b82797d67
Sonu Saha <sonu.saha@progress.com>
2022-09-09 18:45:17 +0530

    TODO: Add test for prompt (CFINSPEC-445)
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

---
+++ components/ruby/spec/chef_licensing/strategy/prompt_spec.rb
@@ -0,0 +1,26 @@
+require "tty-prompt"
+require_relative "../../../lib/chef_licensing/license_key_fetcher/prompt"
+require "logger"
+
+RSpec.describe ChefLicensing::LicenseKeyFetcher::Prompt do
+  let (:output) { StringIO.new }
+  let (:logger) { Logger.new(output) }
+  
+  describe "when the user chooses to input license id" do
+    # TODO: Implement this test
+    it "should do something" do
+    end
+  end
+
+  describe "when the user chooses to generate a license id" do
+    # TODO: Implement this test
+    it "should do something" do
+    end
+  end
+
+  describe "when the user chooses to exit" do
+    # TODO: Implement this test
+    it "should do something" do
+    end
+  end
+end
