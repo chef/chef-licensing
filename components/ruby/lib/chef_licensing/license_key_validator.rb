b79aa59c7ca28cbbc7056b0df22a8a6fd32905d7
Sathish <sbabu@progress.com>
2022-09-13 18:46:06 +0530

    validate or raise error
    Signed-off-by: Sathish <sbabu@progress.com>

--- components/ruby/lib/chef_licensing/license_key_validator.rb
+++ components/ruby/lib/chef_licensing/license_key_validator.rb
@@ -1,16 +1,18 @@
+require_relative "restful_client/v1"
+require_relative "exceptions/invalid_license"
+
 module ChefLicensing
   class LicenseKeyValidator
     attr_reader :license
 
     def initialize(license, restful_client: ChefLicensing::RestfulClient::V1)
-      @license = license.presence || raise(ArgumentError, 'Missing Params: `license`')
+      @license = license.presence || raise(ArgumentError, "Missing Params: `license`")
       @restful_client = restful_client.new
     end
 
     def validate!
       response = restful_client.validate(license)
-      # response outputs
-      #raise ChefLicensing::InvalidLicense
+      response.data || raise(ChefLicensing::InvalidLicense)
     end
 
     private
