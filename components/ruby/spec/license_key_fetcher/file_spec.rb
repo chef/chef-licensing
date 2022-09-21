cb33b13ff8d4ddf15cce10f4ee334a2db5753f3b
Nikita Mathur <nikita.mathur@chef.io>
2022-09-21 14:23:38 +0530

    Changed Dir.tmpdir usage in test specs
    Signed-off-by: Nikita Mathur <nikita.mathur@chef.io>

--- components/ruby/spec/license_key_fetcher/file_spec.rb
+++ components/ruby/spec/license_key_fetcher/file_spec.rb
@@ -15,11 +15,12 @@ RSpec.describe ChefLicensing::LicenseKeyFetcher::File do
 
   describe "#persist" do
     it "stores license key in file" do
-      file_fetcher = ChefLicensing::LicenseKeyFetcher::File.new({ dir: Dir.tmpdir })
-      file_fetcher.persist("2345678", "Test-app", "0.1.0")
-      expect(file_fetcher.fetch).to eq("2345678")
-      expect(file_fetcher.persisted?).to eq(true)
-      FileUtils.rm_rf(Dir.tmpdir)
+      Dir.tmpdir do |tmpdir|
+        file_fetcher = ChefLicensing::LicenseKeyFetcher::File.new({ dir: tmpdir })
+        file_fetcher.persist("2345678", "Test-app", "0.1.0")
+        expect(file_fetcher.fetch).to eq("2345678")
+        expect(file_fetcher.persisted?).to eq(true)
+      end
     end
   end
 end
