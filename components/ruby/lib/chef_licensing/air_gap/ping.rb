332e20ffcd1d5818008700a492b7385789def851
Sonu Saha <sonu.saha@progress.com>
2022-09-26 02:34:27 +0530

    METHOD CHANGE: Change the way of verifying license server url
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- components/ruby/lib/chef_licensing/air_gap/ping.rb
+++ components/ruby/lib/chef_licensing/air_gap/ping.rb
@@ -1,18 +1,23 @@
-require "net/ping"
+require "net/http"
 
 module ChefLicensing
   class AirGap
     class Ping
 
-      attr_reader :host
+      attr_reader :host, :status
 
       def initialize(host)
-        @host = host
+        @host = URI(host)
       end
 
       def verify_ping
-        check = Net::Ping::External.new(host)
-        raise AirGapException, "Unable to ping public licensing server.\nPlease check your internet connectivity." unless check.ping?
+        return @status if @status.is_a? Net::HTTPSuccess
+
+        @status = Net::HTTP.get_response(host)
+
+        raise "Error message: #{@status.message}\nStatus code: #{@status.code}" unless @status.is_a? Net::HTTPSuccess
+      rescue => exception
+        raise AirGapException, "Unable to ping public licensing server.\n#{exception.message}"
       end
     end
   end
