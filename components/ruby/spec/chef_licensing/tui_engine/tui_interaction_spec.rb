16fe77c6f35a73d7a57d0692e714861742860061
Sonu Saha <sonu.saha@progress.com>
2022-09-26 17:36:11 +0530

    TEST: Add test for TUIInteraction (CFINSPEC-463)
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

---
+++ components/ruby/spec/chef_licensing/tui_engine/tui_interaction_spec.rb
@@ -0,0 +1,35 @@
+require "chef_licensing/tui_engine/tui_interaction"
+require_relative "../../spec_helper"
+
+RSpec.describe ChefLicensing::TUIEngine::TUIInteraction do
+
+  let(:config) { {
+    id: "test",
+    messages: ["test"],
+    action: "some_action",
+    paths: [],
+  } }
+  let(:tui_interaction) { described_class.new(config) }
+
+  describe "when a tui_interaction object is instantiated" do
+    it "should have an id field" do
+      expect(tui_interaction.id).to eq("test")
+    end
+
+    it "should have messages field" do
+      expect(tui_interaction.messages).to eq(["test"])
+    end
+
+    it "should have an action field" do
+      expect(tui_interaction.action).to eq("some_action")
+    end
+
+    it "should have paths field" do
+      expect(tui_interaction.paths).to eq([])
+    end
+
+    it "should not have other fields" do
+      expect { tui_interaction.non_existing_field }.to raise_error
+    end
+  end
+end
