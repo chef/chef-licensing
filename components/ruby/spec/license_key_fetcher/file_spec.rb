6266e1d263bf863484ec7614308120710eaf4151
Nikita Mathur <nikita.mathur@chef.io>
2022-09-26 14:00:05 +0530

    Test cases to check persist and license file supported version
    Signed-off-by: Nikita Mathur <nikita.mathur@chef.io>

--- components/ruby/spec/license_key_fetcher/file_spec.rb
+++ components/ruby/spec/license_key_fetcher/file_spec.rb
@@ -3,13 +3,29 @@ require "chef_licensing/license_key_fetcher/file"
 require "tmpdir"
 
 RSpec.describe ChefLicensing::LicenseKeyFetcher::File do
-  let(:fixture_dir) { "../fixtures" }
+  let(:fixture_dir) { "spec/fixtures" }
   let(:license_key_file) { "license_key.yaml" }
+  let(:unsupported_vesion_license_dir) { "spec/fixtures/unsupported_version_license" }
 
   describe "#fetch" do
     it "returns license key from file" do
       file_fetcher = ChefLicensing::LicenseKeyFetcher::File.new({ dir: fixture_dir })
       expect(file_fetcher.fetch).to eq("12345678")
+    end
+
+    it "returns nil when license key is not persisted" do
+      Dir.tmpdir do |tmpdir|
+        file_fetcher = ChefLicensing::LicenseKeyFetcher::File.new({ dir: tmpdir })
+        expect(file_fetcher.fetch).to eq(nil)
+        expect(file_fetcher.persisted?).to eq(false)
+      end
+    end
+
+    it "raises error for unsupported version of license file with wrong version" do
+      Dir.tmpdir do |tmpdir|
+        file_fetcher = ChefLicensing::LicenseKeyFetcher::File.new({ dir: unsupported_vesion_license_dir })
+        expect { file_fetcher.fetch }.to raise_error(RuntimeError, /License File version 10.0.0 not supported./)
+      end
     end
   end
 
