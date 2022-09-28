a5d043e4d1e16a423d7d280aa6ece6b63434e369
Sonu Saha <sonu.saha@progress.com>
2022-09-28 14:50:28 +0530

    FORMAT: Change to class method
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- components/ruby/lib/chef_licensing/air_gap.rb
+++ components/ruby/lib/chef_licensing/air_gap.rb
@@ -8,16 +8,11 @@ module ChefLicensing
     # TODO: Check URL for public licensing server
     LICENSE_SERVER_URL = "https://licensing-acceptance.chef.co/".freeze
 
-    def initialize
+    def self.air_gap_enabled?
       @ping_check = AirGap::Ping.new(LICENSE_SERVER_URL)
       @env_check = AirGap::Environment.new(ENV)
       @argv_check = AirGap::Argument.new(ARGV)
-    end
-
-    def check
-      @env_check.verify_env
-      @argv_check.verify_argv
-      @ping_check.verify_ping
+      @env_check.verify_env || @argv_check.verify_argv || !@ping_check.verify_ping
     rescue AirGapException => exception
       puts exception.message
       # TODO: Exit with some code
