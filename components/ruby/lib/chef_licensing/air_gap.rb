3a01557c14fb4b68b97169faf3865e061c315953
Sonu Saha <sonu.saha@progress.com>
2022-10-11 13:42:08 +0530

    REFACTOR: Use license server url from config
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- components/ruby/lib/chef_licensing/air_gap.rb
+++ components/ruby/lib/chef_licensing/air_gap.rb
@@ -2,20 +2,18 @@ require_relative "air_gap/ping"
 require_relative "air_gap/environment"
 require_relative "air_gap/argument"
 require_relative "air_gap/exception"
+require_relative "config"
 
 module ChefLicensing
-  # TODO: Check URL for public licensing server
-  LICENSE_SERVER_URL = "https://licensing-acceptance.chef.co/".freeze
 
   def self.air_gap_mode_enabled?
-    @ping_check = AirGap::Ping.new(LICENSE_SERVER_URL)
+    @ping_check = AirGap::Ping.new(ChefLicensing::Config::LICENSING_SERVER)
     @env_check = AirGap::Environment.new(ENV)
     @argv_check = AirGap::Argument.new(ARGV)
 
     @env_check.verify_env || @argv_check.verify_argv || !@ping_check.verify_ping
   rescue ChefLicensing::AirGapException => exception
     puts exception.message
-    # TODO: Exit with some code
     exit
   end
 end
