d47d0b14b226b887cd941d67fcc7172a6b0e6b9d
Sonu Saha <sonu.saha@progress.com>
2022-10-11 19:32:54 +0530

    CHORE: Appropriate var name
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- components/ruby/lib/chef_licensing/air_gap/ping.rb
+++ components/ruby/lib/chef_licensing/air_gap/ping.rb
@@ -4,20 +4,20 @@ module ChefLicensing
   class AirGap
     class Ping
 
-      attr_reader :host, :status
+      attr_reader :url, :status
 
-      def initialize(host)
-        @host = URI(host)
+      def initialize(url)
+        @url = URI(url)
       end
 
       def verify_ping
         return @status if @status
 
-        response = Net::HTTP.get_response(host)
+        response = Net::HTTP.get_response(url)
         @status = response.is_a? Net::HTTPSuccess
         @status
       rescue => exception
-        warn "Unable to ping #{host}.\n#{exception.message}"
+        warn "Unable to ping #{url}.\n#{exception.message}"
       end
     end
   end
