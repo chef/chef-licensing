2aab55e77f1d95d8ffaa612d7be23c4098acc9d7
Sathish <sbabu@progress.com>
2022-10-12 12:13:04 +0530

    add ChefLicense abstract APIs
    Signed-off-by: Sathish <sbabu@progress.com>

--- components/ruby/lib/chef_licensing.rb
+++ components/ruby/lib/chef_licensing.rb
@@ -1,6 +1,17 @@
 require "chef_licensing/version"
+require "chef_licensing/license_feature_validator"
+require "chef_licensing/license_key_fetcher"
 
 module ChefLicensing
+  class << self
+    def entitled_feature?(feature)
+      ChefLicensing::LicenseFeatureValidator.validate!(licenses, feature_name: feature)
+    end
 
-  # Your code goes here...
+    # @note no in-memory caching of the licenses so that it fetches updated licenses always
+    def licenses
+      # TODO: remove redundant args
+      ChefLicensing::LicenseKeyFetcher.fetch_and_persist(logger: Logger.new($stdout))
+    end
+  end
 end
