1124ba5c5b3045fc81e2823a711ac033bda35761
Sathish <sbabu@progress.com>
2022-10-13 18:40:26 +0530

    config manager
    Signed-off-by: Sathish <sbabu@progress.com>

--- components/ruby/lib/chef_licensing/config.rb
+++ components/ruby/lib/chef_licensing/config.rb
@@ -1,5 +1,14 @@
 module ChefLicensing
   class Config
-    LICENSING_SERVER = ENV.fetch("CHEF_LICENSING_SERVER", "https://licensing.chef.co/License")
+    class << self
+      attr_accessor :licensing_server
+      attr_writer :logger
+
+      def logger
+        @logger || Logger.new($stdout)
+      end
+    end
+
+    LICENSING_SERVER = self.licensing_server ||= ENV.fetch("CHEF_LICENSING_SERVER", "https://licensing.chef.co/License")
   end
 end
