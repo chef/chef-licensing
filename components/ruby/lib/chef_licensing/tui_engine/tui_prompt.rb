ae3e90c40d0bd4a71fc1784bcf95e9f1788ffa66
Sonu Saha <sonu.saha@progress.com>
2022-10-18 16:40:41 +0530

    CHORE: Add comments (CFINSPEC-463)
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- components/ruby/lib/chef_licensing/tui_engine/tui_prompt.rb
+++ components/ruby/lib/chef_licensing/tui_engine/tui_prompt.rb
@@ -17,42 +17,52 @@ module ChefLicensing
 
       # TBD: Add comments for each prompt type briefly.
 
+      # yes prompts the user with a yes/no question and returns true if the user
+      # answers yes and false if the user answers no.
       def yes(messages)
         message = messages.is_a?(Array) ? messages[0] : messages
 
         @tty_prompt.yes?(message, default: true)
       end
 
+      # say prints the given message to the output stream.
+      # default prompt_type is say.
       def say(messages)
         message = messages.is_a?(Array) ? messages[0] : messages
         @tty_prompt.say(message)
       end
 
+      # ok prints the given message to the output stream in green color.
       def ok(messages)
         message = messages.is_a?(Array) ? messages[0] : messages
         @tty_prompt.ok(message)
       end
 
+      # warn prints the given message to the output stream in yellow color.
       def warn(messages)
         message = messages.is_a?(Array) ? messages[0] : messages
         @tty_prompt.warn(message)
       end
 
+      # error prints the given message to the output stream in red color.
       def error(messages)
         message = messages.is_a?(Array) ? messages[0] : messages
         @tty_prompt.error(message)
       end
 
+      # select prompts the user to select an option from a list of options.
       def select(messages)
         header = messages[0]
         choices_list = messages[1]
         @tty_prompt.select(header, choices_list)
       end
 
+      # enum_select prompts the user to select an option from a list of options.
       def enum_select(messages)
         @tty_prompt.enum_select(messages[0], messages[1])
       end
 
+      # ask prompts the user to enter a value.
       def ask(messages)
         message = messages.is_a?(Array) ? messages[0] : messages
         @tty_prompt.ask(message)
