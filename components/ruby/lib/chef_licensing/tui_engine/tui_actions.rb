f3531c4ea8bdc48e7e73ecd05a86a63341374cc8
Sonu Saha <sonu.saha@progress.com>
2022-10-18 12:14:26 +0530

    FEAT: Rebase & Update license id regex check (CFINSPEC-463)
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- components/ruby/lib/chef_licensing/tui_engine/tui_actions.rb
+++ components/ruby/lib/chef_licensing/tui_engine/tui_actions.rb
@@ -1,8 +1,11 @@
 require_relative "../license_key_validator"
 require_relative "../exceptions/invalid_license"
+require_relative "../license_key_fetcher/base"
 module ChefLicensing
   class TUIEngine
-    class TUIActions
+    # TODO: Is there a better way to use the base class?
+    # Base class is required for constants like LICENSE_KEY_REGEX
+    class TUIActions < LicenseKeyFetcher::Base
 
       def initialize(opts = {}) end
 
@@ -10,8 +13,18 @@ module ChefLicensing
         puts "Welcome you finally got here, atleast you are not a robot"
       end
 
-      def is_license_valid?(inputs)
-        # TODO: Match the regex after pulling and rebasing with main.
+      def is_license_with_valid_pattern?(inputs)
+        license_id = inputs[:ask_for_license_id]
+        if (match = license_id.match(/^#{LICENSE_KEY_REGEX}$/))
+          inputs[:ask_for_license_id] = match[1]
+          true
+        else
+          puts "License pattern should be #{LICENSE_KEY_PATTERN_DESC}"
+          false
+        end
+      end
+
+      def is_license_valid_on_server?(inputs)
         license_id = inputs[:ask_for_license_id]
         puts "License validation in progress..."
         ChefLicensing::LicenseKeyValidator.validate!(license_id)
