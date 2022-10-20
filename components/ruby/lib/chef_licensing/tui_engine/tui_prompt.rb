4e080e578087fa10a64baa35a7a836943d0ccec3
Sonu Saha <sonu.saha@progress.com>
2022-10-20 12:47:05 +0530

    FEAT: Implement timeout_yes prompt and extend all prompts to support prompt_attributes
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- components/ruby/lib/chef_licensing/tui_engine/tui_prompt.rb
+++ components/ruby/lib/chef_licensing/tui_engine/tui_prompt.rb
@@ -1,6 +1,7 @@
 require "timeout" unless defined?(Timeout)
 require "tty-prompt"
 require "logger"
+require_relative "tui_exceptions"
 
 module ChefLicensing
   class TUIEngine
@@ -18,53 +19,73 @@ module ChefLicensing
 
       # yes prompts the user with a yes/no question and returns true if the user
       # answers yes and false if the user answers no.
-      def yes(messages)
+      def yes(messages, prompt_attributes)
         message = messages.is_a?(Array) ? messages[0] : messages
 
-        @tty_prompt.yes?(message, default: true)
+        @tty_prompt.yes?(message)
       end
 
       # say prints the given message to the output stream.
       # default prompt_type is say.
-      def say(messages)
+      def say(messages, prompt_attributes)
         message = messages.is_a?(Array) ? messages[0] : messages
         @tty_prompt.say(message)
       end
 
       # ok prints the given message to the output stream in green color.
-      def ok(messages)
+      def ok(messages, prompt_attributes)
         message = messages.is_a?(Array) ? messages[0] : messages
         @tty_prompt.ok(message)
       end
 
       # warn prints the given message to the output stream in yellow color.
-      def warn(messages)
+      def warn(messages, prompt_attributes)
         message = messages.is_a?(Array) ? messages[0] : messages
         @tty_prompt.warn(message)
       end
 
       # error prints the given message to the output stream in red color.
-      def error(messages)
+      # prompt_attributes = {} is added to extend the prompt in future.
+      def error(messages, prompt_attributes)
         message = messages.is_a?(Array) ? messages[0] : messages
         @tty_prompt.error(message)
       end
 
       # select prompts the user to select an option from a list of options.
-      def select(messages)
+      # prompt_attributes = {} is added to extend the prompt in future.
+      def select(messages, prompt_attributes)
         header = messages[0]
         choices_list = messages[1]
         @tty_prompt.select(header, choices_list)
       end
 
       # enum_select prompts the user to select an option from a list of options.
-      def enum_select(messages)
+      # prompt_attributes = {} is added to extend the prompt in future.
+      def enum_select(messages, prompt_attributes)
         @tty_prompt.enum_select(messages[0], messages[1])
       end
 
       # ask prompts the user to enter a value.
-      def ask(messages)
+      # prompt_attributes = {} is added to extend the prompt in future.
+      def ask(messages, prompt_attributes)
         message = messages.is_a?(Array) ? messages[0] : messages
         @tty_prompt.ask(message)
+      end
+
+      # timeout_yes prompt wraps yes prompt with timeout.
+      def timeout_yes(messages, prompt_attributes)
+        prompt_attributes.transform_keys!(&:to_sym)
+        timeout_duration = prompt_attributes[:timeout_duration] || 60
+
+        Timeout.timeout(timeout_duration, PromptTimeout) do
+          yes(messages, prompt_attributes)
+        rescue PromptTimeout => e
+          error(prompt_attributes[:timeout_message] || "Prompt Timeout", prompt_attributes)
+          # Qs: Why do we unsubscribe from the tty_prompt here?
+          @tty_prompt.unsubscribe(@tty_prompt.reader)
+          logger.error("Timeout error: #{e}")
+          exit!
+        end
       end
     end
   end
