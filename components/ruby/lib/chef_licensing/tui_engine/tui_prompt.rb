552a64c68876b70d27e58e0245b44bfd2e52d9a9
Sonu Saha <sonu.saha@progress.com>
2022-10-20 16:24:50 +0530

    ERB Support: Add support for erb in messages (CFINSPEC-484)
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- components/ruby/lib/chef_licensing/tui_engine/tui_prompt.rb
+++ components/ruby/lib/chef_licensing/tui_engine/tui_prompt.rb
@@ -2,6 +2,7 @@ require "timeout" unless defined?(Timeout)
 require "tty-prompt"
 require "logger"
 require_relative "tui_exceptions"
+require "erb" unless defined?(Erb)
 
 module ChefLicensing
   class TUIEngine
@@ -20,6 +21,7 @@ module ChefLicensing
       # yes prompts the user with a yes/no question and returns true if the user
       # answers yes and false if the user answers no.
       def yes(messages, prompt_attributes)
+        messages = render_messages(messages)
         message = messages.is_a?(Array) ? messages[0] : messages
 
         @tty_prompt.yes?(message)
@@ -28,18 +30,21 @@ module ChefLicensing
       # say prints the given message to the output stream.
       # default prompt_type is say.
       def say(messages, prompt_attributes)
+        messages = render_messages(messages)
         message = messages.is_a?(Array) ? messages[0] : messages
         @tty_prompt.say(message)
       end
 
       # ok prints the given message to the output stream in green color.
       def ok(messages, prompt_attributes)
+        messages = render_messages(messages)
         message = messages.is_a?(Array) ? messages[0] : messages
         @tty_prompt.ok(message)
       end
 
       # warn prints the given message to the output stream in yellow color.
       def warn(messages, prompt_attributes)
+        messages = render_messages(messages)
         message = messages.is_a?(Array) ? messages[0] : messages
         @tty_prompt.warn(message)
       end
@@ -47,6 +52,7 @@ module ChefLicensing
       # error prints the given message to the output stream in red color.
       # prompt_attributes = {} is added to extend the prompt in future.
       def error(messages, prompt_attributes)
+        messages = render_messages(messages)
         message = messages.is_a?(Array) ? messages[0] : messages
         @tty_prompt.error(message)
       end
@@ -54,6 +60,7 @@ module ChefLicensing
       # select prompts the user to select an option from a list of options.
       # prompt_attributes = {} is added to extend the prompt in future.
       def select(messages, prompt_attributes)
+        messages = render_messages(messages)
         header = messages[0]
         choices_list = messages[1]
         @tty_prompt.select(header, choices_list)
@@ -62,18 +69,21 @@ module ChefLicensing
       # enum_select prompts the user to select an option from a list of options.
       # prompt_attributes = {} is added to extend the prompt in future.
       def enum_select(messages, prompt_attributes)
+        messages = render_messages(messages)
         @tty_prompt.enum_select(messages[0], messages[1])
       end
 
       # ask prompts the user to enter a value.
       # prompt_attributes = {} is added to extend the prompt in future.
       def ask(messages, prompt_attributes)
+        messages = render_messages(messages)
         message = messages.is_a?(Array) ? messages[0] : messages
         @tty_prompt.ask(message)
       end
 
       # timeout_yes prompt wraps yes prompt with timeout.
       def timeout_yes(messages, prompt_attributes)
+        messages = render_messages(messages)
         prompt_attributes.transform_keys!(&:to_sym)
         timeout_duration = prompt_attributes[:timeout_duration] || 60
 
@@ -85,6 +95,22 @@ module ChefLicensing
           @tty_prompt.unsubscribe(@tty_prompt.reader)
           logger.error("Timeout error: #{e}")
           exit!
+        end
+      end
+
+      private
+
+      def erb_result(message)
+        ERB.new(message).result(binding)
+      end
+
+      def render_messages(messages)
+        if messages.is_a?(String)
+          erb_result(messages)
+        elsif messages.is_a?(Array)
+          messages.map do |message|
+            render_messages(message)
+          end
         end
       end
     end
