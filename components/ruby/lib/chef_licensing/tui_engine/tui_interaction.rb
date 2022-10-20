8e325ef293ecd682081eb5f621b2b84374c44bfa
Sonu Saha <sonu.saha@progress.com>
2022-10-20 12:44:01 +0530

    FEAT: Extend TUIInteraction to support prompt_attributes (CFINSPEC-483)
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- components/ruby/lib/chef_licensing/tui_engine/tui_interaction.rb
+++ components/ruby/lib/chef_licensing/tui_engine/tui_interaction.rb
@@ -1,12 +1,13 @@
 module ChefLicensing
   class TUIEngine
     class TUIInteraction
-      attr_accessor :id, :messages, :action, :paths, :prompt_type, :response_path_map
+      attr_accessor :id, :messages, :action, :paths, :prompt_type, :response_path_map, :prompt_attributes
       def initialize(opts = {})
         @id = opts[:id]
         @messages = opts[:messages]
         @action = opts[:action]
         @prompt_type = opts[:prompt_type] || "say"
+        @prompt_attributes = opts[:prompt_attributes] || {}
         @response_path_map = opts[:response_path_map]
         @paths = {}
       end
