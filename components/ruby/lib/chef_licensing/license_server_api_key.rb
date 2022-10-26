57fa4519426efdb1fa13b39a2341a8782b95b838
Sathish <sbabu@progress.com>
2022-10-26 17:59:48 +0530

    add support for License Server API Key
    Signed-off-by: Sathish <sbabu@progress.com>

---
+++ components/ruby/lib/chef_licensing/license_server_api_key.rb
@@ -0,0 +1,7 @@
+module ChefLicensing
+  def self.license_server_api_key
+    # TODO - Implement a "strategy"-based system that accepts CHEF_LICENSE_SERVER_API_KEY and --chef-license-server-api-key
+    # For now, we fetch from ENV with NO DEFAULT
+    ENV.fetch("CHEF_LICENSE_SERVER_API_KEY")
+  end
+end
