d041ddbb6b6623174c9c2de519d948cece845782
Sonu Saha <sonu.saha@progress.com>
2022-10-28 00:22:56 +0530

    CHORES: Clean comments and defaults
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- components/ruby/lib/chef_licensing/tui_engine/tui_prompt.rb
+++ components/ruby/lib/chef_licensing/tui_engine/tui_prompt.rb
@@ -16,64 +16,67 @@ module ChefLicensing
         @tty_prompt = TTY::Prompt.new(track_history: false, active_color: :bold, interrupt: :exit, output: output, input: input)
       end
 
-      # TBD: Add comments for each prompt type briefly.
-
       # yes prompts the user with a yes/no question and returns true if the user
       # answers yes and false if the user answers no.
-      def yes(messages, prompt_attributes = {})
+      # prompt_attributes is added to extend the prompt in future.
+      def yes(messages, prompt_attributes)
         message = messages.is_a?(Array) ? messages[0] : messages
 
         @tty_prompt.yes?(message)
       end
 
       # say prints the given message to the output stream.
-      # default prompt_type is say.
-      def say(messages, prompt_attributes = {})
+      # default prompt_type of an interaction is say.
+      # prompt_attributes is added to extend the prompt in future.
+      def say(messages, prompt_attributes)
         message = messages.is_a?(Array) ? messages[0] : messages
         @tty_prompt.say(message)
       end
 
       # ok prints the given message to the output stream in green color.
-      def ok(messages, prompt_attributes = {})
+      # prompt_attributes is added to extend the prompt in future.
+      def ok(messages, prompt_attributes)
         message = messages.is_a?(Array) ? messages[0] : messages
         @tty_prompt.ok(message)
       end
 
       # warn prints the given message to the output stream in yellow color.
-      def warn(messages, prompt_attributes = {})
+      # prompt_attributes is added to extend the prompt in future.
+      def warn(messages, prompt_attributes)
         message = messages.is_a?(Array) ? messages[0] : messages
         @tty_prompt.warn(message)
       end
 
       # error prints the given message to the output stream in red color.
-      # prompt_attributes = {} is added to extend the prompt in future.
-      def error(messages, prompt_attributes = {})
+      # prompt_attributes is added to extend the prompt in future.
+      def error(messages, prompt_attributes)
         message = messages.is_a?(Array) ? messages[0] : messages
         @tty_prompt.error(message)
       end
 
       # select prompts the user to select an option from a list of options.
-      # prompt_attributes = {} is added to extend the prompt in future.
-      def select(messages, prompt_attributes = {})
+      # prompt_attributes is added to extend the prompt in future.
+      def select(messages, prompt_attributes)
         header = messages[0]
         choices_list = messages[1]
         @tty_prompt.select(header, choices_list)
       end
 
       # enum_select prompts the user to select an option from a list of options.
-      # prompt_attributes = {} is added to extend the prompt in future.
-      def enum_select(messages, prompt_attributes = {})
+      # prompt_attributes is added to extend the prompt in future.
+      def enum_select(messages, prompt_attributes)
         @tty_prompt.enum_select(messages[0], messages[1])
       end
 
       # ask prompts the user to enter a value.
-      # prompt_attributes = {} is added to extend the prompt in future.
-      def ask(messages, prompt_attributes = {})
+      # prompt_attributes is added to extend the prompt in future.
+      def ask(messages, prompt_attributes)
         message = messages.is_a?(Array) ? messages[0] : messages
         @tty_prompt.ask(message)
       end
 
       # timeout_yes prompt wraps yes prompt with timeout.
+      # prompt_attributes is added to extend the prompt in future.
       def timeout_yes(messages, prompt_attributes)
         prompt_attributes.transform_keys!(&:to_sym)
         timeout_duration = prompt_attributes[:timeout_duration] || 60
@@ -82,7 +85,6 @@ module ChefLicensing
           yes(messages, prompt_attributes)
         rescue PromptTimeout
           error(prompt_attributes[:timeout_message] || "Prompt Timeout", prompt_attributes)
-          # Qs: Why do we unsubscribe from the tty_prompt here?
           @tty_prompt.unsubscribe(@tty_prompt.reader)
           # TODO: Exit with a meaningful error code.
           output.puts "Timed out!"
