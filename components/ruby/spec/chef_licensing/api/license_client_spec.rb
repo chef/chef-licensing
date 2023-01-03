ad2824a66140a07330e0f662dd84020b3bdbf416
Nikita Mathur <nikita.mathur@chef.io>
2023-01-03 16:06:33 +0530

    Test cases added and document udpated for client API
    Signed-off-by: Nikita Mathur <nikita.mathur@chef.io>

--- components/ruby/spec/chef_licensing/api/license_client_spec.rb
+++ components/ruby/spec/chef_licensing/api/license_client_spec.rb
@@ -12,19 +12,60 @@ RSpec.describe ChefLicensing::Api::LicenseClient do
     "testing_entitlement_id"
   }
 
+  let(:client_data) {
+    {
+      "Cache" => {
+        "LastModified" => "2022-11-01",
+        "EvaluatedOn" => "2022-11-01",
+        "Expires" => "2024-11-01",
+        "CacheControl" => "2022-11-01",
+      },
+      "Client" => {
+        "license" => "Trial",
+        "status" => "Active",
+        "changesTo" => "Grace",
+        "changesOn" => "2024-11-01",
+        "changesIn" => "2 days",
+        "usage" => "Active",
+        "used" => 2,
+        "limit" => 2,
+        "measure" => 2,
+      },
+      "Assets" => [ { "id" => "assetguid1", "name" => "Test Asset 1" }, { "id" => "assetguid2", "name" => "Test Asset 2" } ],
+      "Features" => [ { "id" => "featureguid1", "name" => "Test Feature 1" }, { "id" => "featureguid2", "name" => "Test Feature 2" } ],
+      "Entitlement" => {
+        "id" => "entitlementguid",
+        "name" => "Inspec",
+        "start" => "2022-11-01",
+        "end" => "2024-11-01",
+        "licenses" => 2,
+        "limits" => [ { "measure" => "nodes", "amount" => 2 } ],
+        "entitled" => false,
+      },
+    }
+  }
+
   subject { described_class.client(license_keys: license_keys, entitlement_id: entitlement_id) }
 
   describe ".client" do
     before do
       stub_request(:get, "#{ChefLicensing.license_server_url}/client")
         .with(query: { licenseKeys: license_keys, entitlementId: entitlement_id })
-        .to_return(body: { message: "License Id is valid", status_code: 200 }.to_json,
+        .to_return(body: { data: client_data, status_code: 200 }.to_json,
                    headers: { content_type: "application/json" })
     end
-    # it { is_expected.to be_truthy } TODO after API is available
+    it { is_expected.to be_truthy }
 
-    context "when license is invalid" do
-      # TODO after API is available
+    context "when license client call raises error" do
+      let(:error_message) { "Invalid licenses" }
+      before do
+        stub_request(:get, "#{ChefLicensing.license_server_url}/client")
+        .with(query: { licenseKeys: license_keys, entitlementId: entitlement_id })
+          .to_return(body: { data: false, message: error_message, status_code: 400 }.to_json,
+                     headers: { content_type: "application/json" })
+      end
+
+      it { expect { subject }.to raise_error(ChefLicensing::LicenseClientError, error_message) }
     end
   end
 end
