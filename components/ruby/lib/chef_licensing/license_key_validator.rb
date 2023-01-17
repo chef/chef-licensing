91037db1f51654715c54702857d5f9545bd4dd9c
Nikita Mathur <nikita.mathur@chef.io>
2023-01-17 18:29:14 +0530

    Removed old logic which was going to be used for expiration flow from validator
    Signed-off-by: Nikita Mathur <nikita.mathur@chef.io>

--- components/ruby/lib/chef_licensing/license_key_validator.rb
+++ components/ruby/lib/chef_licensing/license_key_validator.rb
@@ -3,19 +3,11 @@ require_relative "exceptions/invalid_license"
 
 module ChefLicensing
   class LicenseKeyValidator
-    attr_reader :license, :license_keys
+    attr_reader :license
 
     class << self
       def validate!(license, cl_config: nil)
         new(license, cl_config: cl_config).validate!
-      end
-
-      def licenses_expired?(license_keys)
-        new(license_keys).licenses_expired?
-      end
-
-      def licenses_about_to_expire?(license_keys)
-        new(license_keys).licenses_about_to_expire?
       end
     end
 
@@ -27,18 +19,6 @@ module ChefLicensing
     def validate!
       response = restful_client.validate(license)
       response.data || raise(ChefLicensing::InvalidLicense, response.message)
-    end
-
-    def licenses_expired?
-      # TODO api call to validate expiry of inspec license
-      # Dummy boolean value true
-      true
-    end
-
-    def licenses_about_to_expire?
-      # TODO api call to validate about to expire date of inspec license
-      # Dummy boolean value true
-      true
     end
 
     private
