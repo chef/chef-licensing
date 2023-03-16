532aaf56fabd325681430c857f9a81f085b15c13
Sonu Saha <sonu.saha@progress.com>
2023-03-16 13:46:07 +0530

    TEST: Add test coverage for display_overview method
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- components/ruby/spec/list_license_keys_spec.rb
+++ components/ruby/spec/list_license_keys_spec.rb
@@ -176,4 +176,102 @@ RSpec.describe ChefLicensing::ListLicenseKeys do
       expect(output_stream.string).to include("Error occured while fetching license keys from disk")
     end
   end
+
+  describe "when there are license_keys on the system and overview of the license is fetched" do
+    let(:license_keys) {
+      ["tmns-bea68bbb-1e85-44ea-8b98-a654b011174b-4227"]
+    }
+
+    let(:opts_for_llk) {
+      {
+        output: output_stream,
+        license_keys: license_keys,
+      }
+    }
+
+    let(:describe_api_data) {
+      {
+        "license" => [{
+          "licenseKey" => "guid",
+          "serialNumber" => "testing",
+          "name" => "testing",
+          "status" => "active",
+          "start" => "2022-12-02",
+          "end" => "2023-12-02",
+          "limits" => [
+             {
+              "testing" => "software",
+               "id" => "guid",
+               "amount" => 2,
+               "measure" => 2,
+               "used" => 2,
+               "status" => "Active",
+             },
+          ],
+        }],
+        "Assets" => [
+          {
+            "id" => "guid",
+            "name" => "testing",
+            "entitled" => true,
+            "from" => [
+              {
+                  "license" => "guid",
+                  "status" => "expired",
+              },
+            ],
+          }],
+        "Software" => [
+          {
+            "id" => "guid",
+            "name" => "testing",
+            "entitled" => true,
+            "from" => [
+              {
+                  "license" => "guid",
+                  "status" => "expired",
+              },
+            ],
+          }],
+        "Features" => [
+          {
+            "id" => "guid",
+            "name" => "testing",
+            "entitled" => true,
+            "from" => [
+              {
+                  "license" => "guid",
+                  "status" => "expired",
+              },
+            ],
+          }],
+        "Services" => [
+          {
+            "id" => "guid",
+            "name" => "testing",
+            "entitled" => true,
+            "from" => [
+              {
+                  "license" => "guid",
+                  "status" => "expired",
+              },
+            ],
+          }],
+        }
+    }
+
+    before do
+      stub_request(:get, "#{ChefLicensing::Config.license_server_url}/desc")
+        .with(query: { licenseId: license_keys.join(","), entitlementId: ChefLicensing::Config.chef_entitlement_id })
+        .to_return(body: { data: describe_api_data, status_code: 200 }.to_json,
+                   headers: { content_type: "application/json" })
+    end
+
+    it "displays an overview information about the license keys without errors" do
+      expect { described_class.new(opts_for_llk).display_overview }.to_not raise_error
+      expect(output_stream.string).to include("License Details")
+      expect(output_stream.string).to include("Validity         :")
+      expect(output_stream.string).to include("No. of targets   :")
+    end
+  end
 end
