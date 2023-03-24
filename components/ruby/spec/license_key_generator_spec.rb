ecab276d6ab3483e3effc3b88a1a4c78967f5411
Sonu Saha <sonu.saha@progress.com>
2023-03-24 15:10:08 +0530

    TEST: Test free tier license generation
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- components/ruby/spec/license_key_generator_spec.rb
+++ components/ruby/spec/license_key_generator_spec.rb
@@ -54,63 +54,106 @@ RSpec.describe ChefLicensing::LicenseKeyGenerator do
     { 'x-api-key': api_key }
   }
 
-  subject { described_class.generate!(params) }
+  let(:bad_params) {
+    {
+      first_name: "chef_customer",
+      last_name: "inspec",
+      email_id: "xxxxxxxxx",
+      product: "inspec",
+      company: "Chef",
+      phone: "xxxxx_yyyyy",
+    }
+  }
+
+  let(:bad_params_response) {
+    { "data": { "error": "Invalid Email Id" }, "status": 400 }.to_json
+  }
+
+  let(:invalid_api_key) {
+    "dummy token"
+  }
+
+  let(:invalid_api_key_response) {
+    { "message": "Forbidden" }.to_json
+  }
 
   describe ".generate!" do
-    before do
-      stub_request(:post, "#{ChefLicensing::Config.license_server_url}/v1/triallicense")
-        .with(body: payload.to_json, headers: headers)
-        .to_return(body: expected_response,
-                   headers: { content_type: "application/json" })
-    end
-    it { is_expected.to eq(expected_license_key) }
-
-    context "when params are bad" do
-
-      let(:params) {
-        {
-          first_name: "chef_customer",
-          last_name: "inspec",
-          email_id: "xxxxxxxxx",
-          product: "inspec",
-          company: "Chef",
-          phone: "xxxxx_yyyyy",
-        }
-      }
-
-      let(:expected_response) {
-        { "data": { "error": "Invalid Email Id" }, "status": 400 }.to_json
-      }
-
+    context "when params are valid" do
       before do
         stub_request(:post, "#{ChefLicensing::Config.license_server_url}/v1/triallicense")
           .with(body: payload.to_json, headers: headers)
-          .to_return(body: expected_response, headers: { content_type: "application/json" }, status: 400)
+          .to_return(body: expected_response,
+                     headers: { content_type: "application/json" })
       end
 
-      it { expect { subject }.to raise_error(ChefLicensing::LicenseGenerationFailed) }
+      it "should return the license key" do
+        expect(described_class.generate!(params)).to eq(expected_license_key)
+      end
+    end
+
+    context "when params are bad" do
+      before do
+        stub_request(:post, "#{ChefLicensing::Config.license_server_url}/v1/triallicense")
+          .with(body: bad_params.to_json, headers: headers)
+          .to_return(body: bad_params_response, headers: { content_type: "application/json" }, status: 400)
+      end
+
+      it "should raise an error" do
+        expect { described_class.generate!(bad_params) }.to raise_error(ChefLicensing::LicenseGenerationFailed)
+      end
     end
 
     context "when invalid token is set" do
-      let(:api_key) {
-        "dummy token"
-      }
-
-      let(:expected_response) {
-        { "message": "Forbidden" }.to_json
-      }
-
       before do
         ChefLicensing.configure do |config|
-          config.license_server_api_key = api_key
+          config.license_server_api_key = invalid_api_key
         end
 
         stub_request(:post, "#{ChefLicensing::Config.license_server_url}/v1/triallicense")
           .with(body: payload.to_json, headers: headers)
-          .to_return(body: expected_response, headers: { content_type: "application/json" }, status: 403)
+          .to_return(body: invalid_api_key_response, headers: { content_type: "application/json" }, status: 403)
+      end
+      it { expect { described_class.generate!(params) }.to raise_error(ChefLicensing::LicenseGenerationFailed) }
+    end
+  end
+
+  describe ".generate_free_license!" do
+    context "when params are valid" do
+      before do
+        stub_request(:post, "#{ChefLicensing::Config.license_server_url}/v1/freetierlicense")
+          .with(body: payload.to_json, headers: headers)
+          .to_return(body: expected_response,
+                     headers: { content_type: "application/json" })
       end
 
-      it { expect { described_class.generate!(params) }.to raise_error(ChefLicensing::LicenseGenerationFailed) }
+      it "should return the license key" do
+        expect(described_class.generate_free_license!(params)).to eq(expected_license_key)
+      end
+    end
+
+    context "when params are bad" do
+      before do
+        stub_request(:post, "#{ChefLicensing::Config.license_server_url}/v1/freetierlicense")
+          .with(body: bad_params.to_json, headers: headers)
+          .to_return(body: bad_params_response, headers: { content_type: "application/json" }, status: 400)
+      end
+
+      it "should raise an error" do
+        expect { described_class.generate_free_license!(bad_params) }.to raise_error(ChefLicensing::LicenseGenerationFailed)
+      end
+    end
+
+    context "when invalid token is set" do
+      before do
+        ChefLicensing.configure do |config|
+          config.license_server_api_key = invalid_api_key
+        end
+
+        stub_request(:post, "#{ChefLicensing::Config.license_server_url}/v1/freetierlicense")
+          .with(body: payload.to_json, headers: headers)
+          .to_return(body: invalid_api_key_response, headers: { content_type: "application/json" }, status: 403)
+      end
+      it { expect { described_class.generate_free_license!(params) }.to raise_error(ChefLicensing::LicenseGenerationFailed) }
     end
   end
 end
