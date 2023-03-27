4fd6f6d7b4f71b1a2484a412c92edb1e19355e7e
Vasu1105 <vasundhara.jagdale@chef.io>
2023-03-27 21:51:04 +0530

    Fix for  Invalid argument @ dir_s_mkdir - [C: (Errno::EINVAL)] on windows
    Signed-off-by: Vasu1105 <vasundhara.jagdale@chef.io>

--- components/ruby/lib/chef-licensing/restful_client/base.rb
+++ components/ruby/lib/chef-licensing/restful_client/base.rb
@@ -119,7 +119,7 @@ module ChefLicensing
       private
 
       def get_connection
-        store = ::ActiveSupport::Cache.lookup_store(:file_store, [Dir.tmpdir])
+        store = ::ActiveSupport::Cache.lookup_store(:file_store, Dir.tmpdir)
         Faraday.new(url: ChefLicensing::Config.license_server_url) do |config|
           config.request :json
           config.response :json, parser_options: { object_class: OpenStruct }
