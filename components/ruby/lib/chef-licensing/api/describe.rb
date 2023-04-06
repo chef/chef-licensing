38df19efd4d7945f8650c4afbb7f544078b1ad97
Sonu Saha <sonu.saha@progress.com>
2023-04-07 02:42:03 +0530

    REFACTOR: Use guard clause and safe navigation operator
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- components/ruby/lib/chef-licensing/api/describe.rb
+++ components/ruby/lib/chef-licensing/api/describe.rb
@@ -22,30 +22,30 @@ module ChefLicensing
 
       def list
         response = restful_client.describe(license_keys: license_keys.join(","), entitlement_id: ChefLicensing::Config.chef_entitlement_id)
-        if response.data
-          list_of_licenses = []
 
-          response.data.license.each do |license|
+        raise(ChefLicensing::DescribeError, response.message) unless response.data
 
-            # license object created to be fed to parser
-            license_object = OpenStruct.new({
-              "license" => license,
-              "assets" => response.data.Assets,
-              "features" => response.data.Features,
-              "software" => response.data.Software,
-            })
+        raise(ChefLicensing::DescribeError, "No license details found for the given license keys") unless response.data.license
 
-            list_of_licenses << ChefLicensing::License.new(
-              data: license_object,
-              product_name: ChefLicensing::Config.chef_product_name,
-              api_parser: ChefLicensing::Api::Parser::Describe
-            )
-          end
-          # returns list of license data model object
-          list_of_licenses
-        else
-          raise(ChefLicensing::DescribeError, response.message)
+        list_of_licenses = []
+
+        response.data&.license&.each do |license|
+          # license object created to be fed to parser
+          license_object = OpenStruct.new({
+            "license" => license,
+            "assets" => response.data.Assets,
+            "features" => response.data.Features,
+            "software" => response.data.Software,
+          })
+
+          list_of_licenses << ChefLicensing::License.new(
+            data: license_object,
+            product_name: ChefLicensing::Config.chef_product_name,
+            api_parser: ChefLicensing::Api::Parser::Describe
+          )
         end
+        # returns list of license data model object
+        list_of_licenses
       end
 
       private
