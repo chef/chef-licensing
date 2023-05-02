1b6018323a02ddabdc323a9079ddb551c1a73c3f
Sonu Saha <sonu.saha@progress.com>
2023-05-02 12:59:26 +0530

    TEST: Attempt for TUI Entry UX test Checks for the traversed interaction in order Checks for few important messages that are displayed to the user
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

---
+++ components/ruby/spec/chef_licensing_ux_spec.rb
@@ -0,0 +1,180 @@
+require "chef-licensing/tui_engine/tui_engine"
+require "chef-licensing/config"
+require "spec_helper"
+require "stringio"
+require "chef-licensing"
+require "tty-prompt"
+require "tty/prompt/test"
+
+RSpec.describe ChefLicensing::TUIEngine do
+  let(:interaction_file) { File.join("lib/chef-licensing/license_key_fetcher", "chef_licensing_interactions.yaml") }
+  let(:output) { StringIO.new }
+  let(:logger) { Logger.new(output) }
+  let(:license_key) { "tmns-58555821-925e-4a27-8fdc-e79dae5a425b-1234" }
+
+  before do
+    ChefLicensing.configure do |config|
+      config.logger = logger
+      config.output = output
+      config.license_server_url = "http://localhost-license-server/License"
+      config.license_server_api_key = "xDblv65Xt84wULmc8qTN78a3Dr2OuuKxa6GDvb67"
+      config.air_gap_status = false
+      config.chef_product_name = "inspec"
+      config.chef_entitlement_id = "3ff52c37-e41f-4f6c-ad4d-365192205968"
+    end
+  end
+
+  context "ux for tui entry - user enters a valid license id" do
+    subject(:prompt) { TTY::Prompt::Test.new }
+
+    let(:opts) {
+      {
+        prompt: prompt,
+        interaction_file: interaction_file,
+      }
+    }
+
+    let(:start_interaction) { :start }
+    before do
+      prompt.input << "\n"
+      prompt.input << "tmns-58555821-925e-4a27-8fdc-e79dae5a425b-1234"
+      prompt.input << "\n"
+      prompt.input.rewind
+    end
+
+    before do
+      stub_request(:get, "#{ChefLicensing::Config.license_server_url}/v1/validate")
+        .with(query: { licenseId: license_key, version: 2 })
+        .to_return(body: { data: true, message: "License Id is valid", status_code: 200 }.to_json,
+                   headers: { content_type: "application/json" })
+    end
+
+    let(:client_data) {
+      {
+        "cache": {
+          "lastModified": "2023-01-16T12:05:40Z",
+          "evaluatedOn": "2023-01-16T12:07:20.114370692Z",
+          "expires": "2023-01-17T12:07:20.114370783Z",
+          "cacheControl": "private,max-age:42460",
+        },
+        "client" => {
+          "license" => "Trial",
+          "status" => "Active",
+          "changesTo" => "Grace",
+          "changesOn" => "2024-11-01",
+          "changesIn" => "2 days",
+          "usage" => "Active",
+          "used" => 2,
+          "limit" => 2,
+          "measure" => 2,
+        },
+        "assets" => [ { "id" => "assetguid1", "name" => "Test Asset 1" }, { "id" => "assetguid2", "name" => "Test Asset 2" } ],
+        "features" => [ { "id" => "featureguid1", "name" => "Test Feature 1" }, { "id" => "featureguid2", "name" => "Test Feature 2" } ],
+        "entitlement" => {
+          "id" => "3ff52c37-e41f-4f6c-ad4d-365192205968",
+          "name" => "Inspec",
+          "start" => "2022-11-01",
+          "end" => "2024-11-01",
+          "licenses" => 2,
+          "limits" => [ { "measure" => "nodes", "amount" => 2 } ],
+          "entitled" => false,
+        },
+      }
+    }
+
+    let(:describe_api_data) {
+      {
+        "license" => [{
+          "licenseKey" => "guid",
+          "serialNumber" => "testing",
+          "name" => "testing",
+          "status" => "active",
+          "start" => "2022-12-02",
+          "end" => "2023-12-02",
+          "limits" => [
+             {
+              "testing" => "software",
+               "id" => "guid",
+               "amount" => 2,
+               "measure" => 2,
+               "used" => 2,
+               "status" => "Active",
+             },
+          ],
+        }],
+        "Assets" => [
+          {
+            "id" => "guid",
+            "name" => "testing",
+            "entitled" => true,
+            "from" => [
+              {
+                  "license" => "guid",
+                  "status" => "expired",
+              },
+            ],
+          }],
+        "Software" => [
+          {
+            "id" => "guid",
+            "name" => "testing",
+            "entitled" => true,
+            "from" => [
+              {
+                  "license" => "guid",
+                  "status" => "expired",
+              },
+            ],
+          }],
+        "Features" => [
+          {
+            "id" => "guid",
+            "name" => "testing",
+            "entitled" => true,
+            "from" => [
+              {
+                  "license" => "guid",
+                  "status" => "expired",
+              },
+            ],
+          }],
+        "Services" => [
+          {
+            "id" => "guid",
+            "name" => "testing",
+            "entitled" => true,
+            "from" => [
+              {
+                  "license" => "guid",
+                  "status" => "expired",
+              },
+            ],
+          }],
+        }
+    }
+
+    before do
+      stub_request(:get, "#{ChefLicensing::Config.license_server_url}/client")
+        .with(query: { licenseId: license_key, entitlementId: ChefLicensing::Config.chef_entitlement_id })
+        .to_return(body: { data: client_data, status_code: 200 }.to_json,
+                     headers: { content_type: "application/json" })
+    end
+
+    before do
+      stub_request(:get, "#{ChefLicensing::Config.license_server_url}/desc")
+        .with(query: { licenseId: license_key, entitlementId: ChefLicensing::Config.chef_entitlement_id })
+        .to_return(body: { data: describe_api_data, status_code: 200 }.to_json,
+                   headers: { content_type: "application/json" })
+    end
+
+    let(:tui_engine) { described_class.new(opts) }
+
+    it "exits successfully traversing through the interactions in expected order" do
+      expect { tui_engine.run_interaction(start_interaction) }.to_not raise_error
+      expect(tui_engine.traversed_interaction).to eq(%i{start ask_if_user_has_license_id ask_for_license_id validate_license_id_pattern validate_license_id_with_api validate_license_restriction validation_success display_license_info fetch_license_id})
+      expect(prompt.output.string).to include("I already have a license ID")
+      expect(prompt.output.string).to include("Please enter your License ID:")
+      expect(prompt.output.string).to include("License validated successfully")
+    end
+  end
+end
