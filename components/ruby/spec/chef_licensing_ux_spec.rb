12c5304d7a4afb0422e99144446046e0e79ee66f
Sonu Saha <sonu.saha@progress.com>
2023-05-02 13:15:39 +0530

    REFACTOR: Minor refactoring with the test
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- components/ruby/spec/chef_licensing_ux_spec.rb
+++ components/ruby/spec/chef_licensing_ux_spec.rb
@@ -5,12 +5,15 @@ require "stringio"
 require "chef-licensing"
 require "tty-prompt"
 require "tty/prompt/test"
+require "json"
 
 RSpec.describe ChefLicensing::TUIEngine do
   let(:interaction_file) { File.join("lib/chef-licensing/license_key_fetcher", "chef_licensing_interactions.yaml") }
   let(:output) { StringIO.new }
   let(:logger) { Logger.new(output) }
   let(:license_key) { "tmns-58555821-925e-4a27-8fdc-e79dae5a425b-1234" }
+  let(:valid_client_api_data) { JSON.parse(File.read("spec/fixtures/api_response_data/valid_client_api_response.json")) }
+  let(:valid_describe_api_data) { JSON.parse(File.read("spec/fixtures/api_response_data/valid_describe_api_response.json")) }
 
   before do
     ChefLicensing.configure do |config|
@@ -35,6 +38,10 @@ RSpec.describe ChefLicensing::TUIEngine do
     }
 
     let(:start_interaction) { :start }
+
+    # user press enters to select I already have a license ID
+    # user enters a valid license key
+    # user presses enter to continue
     before do
       prompt.input << "\n"
       prompt.input << "tmns-58555821-925e-4a27-8fdc-e79dae5a425b-1234"
@@ -47,124 +54,16 @@ RSpec.describe ChefLicensing::TUIEngine do
         .with(query: { licenseId: license_key, version: 2 })
         .to_return(body: { data: true, message: "License Id is valid", status_code: 200 }.to_json,
                    headers: { content_type: "application/json" })
-    end
 
-    let(:client_data) {
-      {
-        "cache": {
-          "lastModified": "2023-01-16T12:05:40Z",
-          "evaluatedOn": "2023-01-16T12:07:20.114370692Z",
-          "expires": "2023-01-17T12:07:20.114370783Z",
-          "cacheControl": "private,max-age:42460",
-        },
-        "client" => {
-          "license" => "Trial",
-          "status" => "Active",
-          "changesTo" => "Grace",
-          "changesOn" => "2024-11-01",
-          "changesIn" => "2 days",
-          "usage" => "Active",
-          "used" => 2,
-          "limit" => 2,
-          "measure" => 2,
-        },
-        "assets" => [ { "id" => "assetguid1", "name" => "Test Asset 1" }, { "id" => "assetguid2", "name" => "Test Asset 2" } ],
-        "features" => [ { "id" => "featureguid1", "name" => "Test Feature 1" }, { "id" => "featureguid2", "name" => "Test Feature 2" } ],
-        "entitlement" => {
-          "id" => "3ff52c37-e41f-4f6c-ad4d-365192205968",
-          "name" => "Inspec",
-          "start" => "2022-11-01",
-          "end" => "2024-11-01",
-          "licenses" => 2,
-          "limits" => [ { "measure" => "nodes", "amount" => 2 } ],
-          "entitled" => false,
-        },
-      }
-    }
-
-    let(:describe_api_data) {
-      {
-        "license" => [{
-          "licenseKey" => "guid",
-          "serialNumber" => "testing",
-          "name" => "testing",
-          "status" => "active",
-          "start" => "2022-12-02",
-          "end" => "2023-12-02",
-          "limits" => [
-             {
-              "testing" => "software",
-               "id" => "guid",
-               "amount" => 2,
-               "measure" => 2,
-               "used" => 2,
-               "status" => "Active",
-             },
-          ],
-        }],
-        "Assets" => [
-          {
-            "id" => "guid",
-            "name" => "testing",
-            "entitled" => true,
-            "from" => [
-              {
-                  "license" => "guid",
-                  "status" => "expired",
-              },
-            ],
-          }],
-        "Software" => [
-          {
-            "id" => "guid",
-            "name" => "testing",
-            "entitled" => true,
-            "from" => [
-              {
-                  "license" => "guid",
-                  "status" => "expired",
-              },
-            ],
-          }],
-        "Features" => [
-          {
-            "id" => "guid",
-            "name" => "testing",
-            "entitled" => true,
-            "from" => [
-              {
-                  "license" => "guid",
-                  "status" => "expired",
-              },
-            ],
-          }],
-        "Services" => [
-          {
-            "id" => "guid",
-            "name" => "testing",
-            "entitled" => true,
-            "from" => [
-              {
-                  "license" => "guid",
-                  "status" => "expired",
-              },
-            ],
-          }],
-        }
-    }
-
-    before do
       stub_request(:get, "#{ChefLicensing::Config.license_server_url}/client")
         .with(query: { licenseId: license_key, entitlementId: ChefLicensing::Config.chef_entitlement_id })
-        .to_return(body: { data: client_data, status_code: 200 }.to_json,
-                     headers: { content_type: "application/json" })
-    end
+        .to_return(body: { data: valid_client_api_data, status_code: 200 }.to_json,
+                  headers: { content_type: "application/json" })
 
-    before do
       stub_request(:get, "#{ChefLicensing::Config.license_server_url}/desc")
         .with(query: { licenseId: license_key, entitlementId: ChefLicensing::Config.chef_entitlement_id })
-        .to_return(body: { data: describe_api_data, status_code: 200 }.to_json,
-                   headers: { content_type: "application/json" })
+        .to_return(body: { data: valid_describe_api_data, status_code: 200 }.to_json,
+                  headers: { content_type: "application/json" })
     end
 
     let(:tui_engine) { described_class.new(opts) }
