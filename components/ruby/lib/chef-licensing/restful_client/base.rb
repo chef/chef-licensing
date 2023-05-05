18683a89e0ff3eef2aa90b4a3159781e14750643
Sonu Saha <sonu.saha@progress.com>
2023-05-05 12:01:54 +0530

    API: Update licensing server's endpoint
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- components/ruby/lib/chef-licensing/restful_client/base.rb
+++ components/ruby/lib/chef-licensing/restful_client/base.rb
@@ -14,8 +14,8 @@ module ChefLicensing
 
       END_POINTS = {
         VALIDATE: "validate",
-        GENERATE_TRIAL_LICENSE: "triallicense",
-        GENERATE_FREE_LICENSE: "freetierlicense",
+        GENERATE_TRIAL_LICENSE: "license/trial",
+        GENERATE_FREE_LICENSE: "license/free",
         FEATURE_BY_NAME: "license-service/featurebyname",
         FEATURE_BY_ID: "license-service/featurebyid",
         ENTITLEMENT_BY_NAME: "license-service/entitlementbyname",
