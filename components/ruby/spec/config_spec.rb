c6939b83484b2e67fd6477b392fe66b329cd3c5f
Sonu Saha <sonu.saha@progress.com>
2023-06-01 18:03:17 +0530

    TEST: Add test to fetch server url from file when not present in arg or env
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- components/ruby/spec/config_spec.rb
+++ components/ruby/spec/config_spec.rb
@@ -1,7 +1,6 @@
 require "chef-licensing/config"
 require "logger"
 require "chef-licensing"
-require "spec_helper"
 require "stringio"
 require "fileutils" unless defined?(FileUtils)
 
@@ -12,8 +11,7 @@ RSpec.describe ChefLicensing::Config do
     let(:logger) { Logger.new(output) }
 
     context "when values are set via block" do
-      it "sets the values" do
-
+      before do
         ChefLicensing.configure do |config|
           config.logger = logger
           config.output = output
@@ -23,7 +21,9 @@ RSpec.describe ChefLicensing::Config do
           config.chef_entitlement_id = "3ff52c37-e41f-4f6c-ad4d-365192205968"
           config.chef_executable_name = "inspec"
         end
+      end
 
+      it "sets the values" do
         expect(ChefLicensing::Config.license_server_url).to eq("http://localhost-license-server/License")
         expect(ChefLicensing::Config.logger).to eq(logger)
         expect(ChefLicensing::Config.output).to eq(output)
@@ -32,15 +32,30 @@ RSpec.describe ChefLicensing::Config do
         expect(ChefLicensing::Config.chef_entitlement_id).to eq("3ff52c37-e41f-4f6c-ad4d-365192205968")
         expect(ChefLicensing::Config.chef_executable_name).to eq("inspec")
       end
-    end
 
-    context "when values are set via environment variables" do
-    end
-
-    context "when values are set via command line arguments" do
+      after do
+        ChefLicensing.configure do |config|
+          config.logger = nil
+          config.output = nil
+          config.license_server_url = nil
+          config.air_gap_status = nil
+          config.chef_product_name = nil
+          config.chef_entitlement_id = nil
+          config.chef_executable_name = nil
+        end
+      end
     end
 
     context "fetching values from licenses.yaml file for license server url" do
+      let(:opts) {
+        {
+          dir: "spec/fixtures/license_file_with_server_url",
+        }
+      }
+
+      it "fetches the value from licenses.yaml file" do
+        expect(ChefLicensing::Config.license_server_url(opts)).to eq("https://custom-licensing-server.com/License")
+      end
     end
   end
 end
