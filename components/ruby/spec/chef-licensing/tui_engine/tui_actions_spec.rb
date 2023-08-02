55f7be19baea274283a4fc3148ad90e2ff88c1e7
Sonu Saha <98935583+ahasunos@users.noreply.github.com>
2023-08-02 18:54:07 +0530

    Update regex for company name validation (#152)
    * REGEX: Update regex for company name validation
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * TEST: Include test to verify the new regex
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * TEST: Regex could be updated to handle such invalid names
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    * LINT: Fix lint offense
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>
    
    ---------
    
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- components/ruby/spec/chef-licensing/tui_engine/tui_actions_spec.rb
+++ components/ruby/spec/chef-licensing/tui_engine/tui_actions_spec.rb
@@ -9,4 +9,51 @@ RSpec.describe ChefLicensing::TUIEngine::TUIActions do
       expect(tui_actions).to be_a(ChefLicensing::TUIEngine::TUIActions)
     end
   end
+
+  describe "#is_company_name_valid?" do
+    valid_company_names = [
+      "Chef Software",
+      "InSpec",
+      "Progress Softwares",
+      "Company!!!",
+      "Company 123",
+      "Company&Co",
+      "Company_123",
+      "A Company",
+      "A Company 123",
+      "@ Company",
+    ]
+
+    valid_company_names.each do |company_name|
+      it "should return true for valid company name '#{company_name}'" do
+        expect(tui_actions.is_company_name_valid?({ gather_user_company_for_license_generation: company_name })).to be_truthy
+      end
+    end
+
+    invalid_company_names = [
+      "A", # too short
+      "AB", # too short
+      " A ", # leading space
+      " ABC", # leading space
+      "", # empty
+      " ", # empty
+      "A" * 21, # too long, max 20
+    ]
+
+    invalid_company_names.each do |company_name|
+      it "should return false for invalid company name '#{company_name}'" do
+        expect(tui_actions.is_company_name_valid?({ gather_user_company_for_license_generation: company_name })).to be_falsey
+      end
+    end
+
+    unhandled_company_names = [
+      "A  ", # 3 characters, but by using space
+    ]
+
+    unhandled_company_names.each do |company_name|
+      it "should return false but returns true for unhandled company name '#{company_name}'" do
+        expect(tui_actions.is_company_name_valid?({ gather_user_company_for_license_generation: company_name })).to be_truthy
+      end
+    end
+  end
 end
