26771934644ac83f210bee0de9245f02fe750a89
Sonu Saha <sonu.saha@progress.com>
2023-08-22 13:20:29 +0530

    CACHE: Implement caching for license object for an aggregated license keys
    Signed-off-by: Sonu Saha <sonu.saha@progress.com>

--- components/ruby/lib/chef-licensing/api/client.rb
+++ components/ruby/lib/chef-licensing/api/client.rb
@@ -2,6 +2,7 @@ require_relative "../restful_client/v1"
 require_relative "../exceptions/client_error"
 require_relative "../license"
 require_relative "../config"
+require_relative "../restful_client/cache_manager"
 
 module ChefLicensing
   module Api
@@ -17,11 +18,26 @@ module ChefLicensing
       def initialize(opts = {})
         @license_keys = opts[:license_keys] || raise(ArgumentError, "Missing Params: `license_keys`")
         @restful_client = opts[:restful_client] ? opts[:restful_client].new : ChefLicensing::RestfulClient::V1.new
+        @cache_manager = opts[:cache_manager] || ChefLicensing::RestfulClient::CacheManager.new
       end
 
       def info
+        cache_key = license_keys.join(",")
+        @cache_manager.fetch(cache_key) do
+          license = fetch_license
+          @cache_manager.store(cache_key, license, ttl_cache)
+          license
+        end
+      end
+
+      private
+
+      attr_reader :restful_client, :ttl_cache
+
+      def fetch_license
         response = restful_client.client(license_keys: license_keys.join(","), entitlement_id: ChefLicensing::Config.chef_entitlement_id)
         if response.data
+          @ttl_cache = response.data.cache.expires
           ChefLicensing::License.new(
             data: response.data,
             api_parser: ChefLicensing::Api::Parser::Client
@@ -30,10 +46,6 @@ module ChefLicensing
           raise(ChefLicensing::ClientError, response.message)
         end
       end
-
-      private
-
-      attr_reader :restful_client
     end
   end
 end
