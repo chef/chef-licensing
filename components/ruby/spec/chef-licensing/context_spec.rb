0521dcde209c13ca1a1bcedd2ac5ed5e469420be
Nikita Mathur <Nik08@users.noreply.github.com>
2024-05-06 16:46:01 +0530

    CHEF-7258 License context caching and fetching (#171)
    * License context caching and fetching
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    * Unit test case for license context added
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>
    
    ---------
    
    Signed-off-by: Nik08 <nikita.mathur@progress.com>

---
+++ components/ruby/spec/chef-licensing/context_spec.rb
@@ -0,0 +1,42 @@
+require "chef-licensing/context"
+require "chef-licensing"
+
+RSpec.describe ChefLicensing::Context do
+  let(:output) { StringIO.new }
+  let(:logger) { Logger.new(output) }
+  let(:client_api_data) { JSON.parse(File.read("spec/fixtures/api_response_data/valid_client_api_response.json")) }
+  let(:argv) { [] }
+  let(:env) { {} }
+  let(:opts) {
+    {
+      dir: Dir.mktmpdir,
+      output: output,
+      logger: logger,
+      argv: argv,
+      env: env,
+    }
+  }
+
+  before do
+    ChefLicensing.configure do |config|
+      config.chef_product_name = "inspec"
+      config.chef_entitlement_id = "3ff52c37-e41f-4f6c-ad4d-365192205968"
+      config.license_server_url = "http://localhost-license-server/License"
+      config.is_local_license_service = nil
+    end
+    ChefLicensing::Context.current_context = nil
+    stub_request(:get, "#{ChefLicensing::Config.license_server_url}/v1/listLicenses")
+      .to_return(body: { data: ["tmns-0f76efaf-b45b-4d92-86b2-2d144ce73dfa-150"], status_code: 200 }.to_json,
+    headers: { content_type: "application/json" })
+    stub_request(:get, "#{ChefLicensing::Config.license_server_url}/v1/client")
+      .with(query: { licenseId: "tmns-0f76efaf-b45b-4d92-86b2-2d144ce73dfa-150", entitlementId: ChefLicensing::Config.chef_entitlement_id })
+      .to_return(body: { data: client_api_data, status_code: 200 }.to_json,
+              headers: { content_type: "application/json" })
+  end
+
+  describe "Fetching license context" do
+    it "returns license context succesfully" do
+      expect(ChefLicensing::Context.license.class).to eq(ChefLicensing::License)
+    end
+  end
+end
