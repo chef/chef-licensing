5396bd0a8a26e9ee591e7baaf651088801a14b32
Nik08 <nikita.mathur@progress.com>
2025-09-09 16:55:27 +0530

    Fixed logic to avoid unrequired license file write operation
    Signed-off-by: Nik08 <nikita.mathur@progress.com>

--- components/ruby/lib/chef-licensing/license_key_fetcher/file.rb
+++ components/ruby/lib/chef-licensing/license_key_fetcher/file.rb
@@ -140,6 +140,10 @@ module ChefLicensing
           load_basic_license_data_to_contents(url, [])
         elsif @contents && license_server_url_from_system && license_server_url_from_system != @contents[:license_server_url]
           @contents[:license_server_url] = license_server_url_from_system
+        else
+          # Nothing to change in the file
+          @license_server_url = @contents[:license_server_url]
+          return @license_server_url
         end
 
         # Ensure the license server URL is returned to the caller in all cases
